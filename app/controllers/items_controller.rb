4b665897bdf2bb5a9f1dd3484bc305c28338c94c
Landon Whitesides <whitesideslandon@gmail.com>
2020-11-18 22:48:17 -0700

    added CRUD for items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,65 @@
 class ItemsController < ApplicationController
   def index
+    @department = Department.find(params[:department_id])
+    @items = @department.items
   end
 
   def show
+    @department = Department.find(params[:department_id])
+    @item = @department.items.find(params[:id])
   end
 
   def new
+    @department = Department.find(params[:department_id])
+    @item = @department.new()
+    render partial: "form"
+
+  end
+
+  def create
+    @department = Department.find(params[:department_id])
+    @item = @department.new(item_params)
+
+    if @item.save
+      redirect_to department_items_path(@department_id)
+    else
+      render :new
+    end
+
   end
 
   def edit
+    @department = Department.find(params[:department_id])
+    @item = @department.items.find(params[:id])
+    render partial: "form"
   end
+
+  def update
+    @department = Department.find(params[:department_id])
+    @item = @department.items.find(params[:id])
+
+    if @item.update(item_params)
+      redirect_to department_item_path(@department,@item)
+    else
+      render :edit
+    end
+    
+  end
+
+  def destroy
+    @department = Department.find(params[:department_id])
+    @item = @department.items.find(params[:id])
+
+    @item.destroy
+    redirect_to redirect_to department_items_path(@department_id)
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :description, :price, :quantity)
+  end
+
 end
+
